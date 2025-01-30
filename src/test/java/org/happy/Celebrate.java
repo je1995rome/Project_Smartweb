282b230026402aac7659a2038f66a64b750e079d
Je1995rome <je1995rome@gmail.com>
2025-01-30 16:20:39 +0530

    New updated celebrate code

--- src/test/java/org/happy/Celebrate.java
+++ src/test/java/org/happy/Celebrate.java
@@ -2,4 +2,11 @@ package org.happy;
 
 public class Celebrate {
 
+	
+	public static void main(String[] args) {
+		System.out.println("happy brithday");
+		
+		System.out.println("Reno2509");
+	}
 }
+ 
