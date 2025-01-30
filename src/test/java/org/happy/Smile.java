1ca5d88043f1b9bc8bb5036486c24911513b6355
Je1995rome <je1995rome@gmail.com>
2025-01-30 14:38:31 +0530

    smile code

---
+++ src/test/java/org/happy/Smile.java
@@ -0,0 +1,13 @@
+package org.happy;
+
+public class Smile {
+	public static void main(String[] args) {
+		System.out.println("smile1");
+		System.out.println("smile2");
+		System.out.println("smile3");
+		
+		
+		
+	}
+
+}
